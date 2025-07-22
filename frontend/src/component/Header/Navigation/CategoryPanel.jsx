496d47a8fa42f01f7fdfe7a69688c558c37e12af
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-07-22 19:15:06 +0530

    submenu

--- frontend/src/component/Header/Navigation/CategoryPanel.jsx
+++ frontend/src/component/Header/Navigation/CategoryPanel.jsx
@@ -12,39 +12,73 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { IoClose } from "react-icons/io5";
 import { FaRegPlusSquare } from "react-icons/fa";
+import '../Navigation/Navbar.css'
+import { Link } from 'react-router-dom';
 
 const CategoryPanel = (props) => {
 
-     
+
 
   const toggleDrawer = (newOpen) => () => {
     props.setIsOpenCatPanel(newOpen)
   };
 
-   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" >
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" className="categoryPanel" >
 
-        <h3 className='p 3 text-[16px] font-[500] flex items-center justify-between'>
-          Shop By Category <IoClose  onClick={toggleDrawer(false)} className='cursor-pointer text-[20px]'/> 
-          </h3>
-     
-       <div className='scroll'>
+      <h3 className='p 3 text-[16px] font-[500] flex items-center justify-between'>
+        Shop By Category <IoClose onClick={toggleDrawer(false)} className='cursor-pointer text-[20px] ' />
+      </h3>
+
+      <div className='scroll'>
 
         <ul className='w-full'>
           <li className='list-none flex items-center relative'>
+                 <Link to='/'className='w-full'>
+            <Button className='w-full !text-left !justify-start !px-3 !text-black'>
+              Fashion
+              </Button>
+              </Link>
+            <FaRegPlusSquare className='absolute top-[10px] right-[15px] cursor-pointer '  />
 
-            <Button className='w-full !text-left !justify-start !px-3 !text-black'>Fashion</Button>
-            <FaRegPlusSquare className='absolute top-[10px] right-[15px]' />
+
+            <ul className='submenu absolute top-[100%] left-[0%] w-full pl-3  '>
+              <li className='list-none relative'>
+                  <Link to='/' className='w-full'>
+                <Button className='w-full !text-left !justify-start !px-3 !text-black'>
+                  Appreal
+                  </Button>
+                  </Link>
+                <FaRegPlusSquare className='absolute top-[10px] right-[15px] cursor-pointer ' />
+
+
+                  <ul className='submenu absolute top-[100%] left-[0%] w-full pl-3  '>
+              <li className='list-none relative'>
+
+                <Link to='/' className=' link w-full !text-left !justify-start !px-3 transition text-[14px]'>
+                  Appreal
+                  </Link>
+               
+
+              </li>
+            </ul>
+
+   
+
+              </li>
+            </ul>
+
+
           </li>
         </ul>
-       </div>
+      </div>
 
     </Box>
   );
 
   return (
     <div>
-      
+
       <Drawer open={props.isOpenCatPanel} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
